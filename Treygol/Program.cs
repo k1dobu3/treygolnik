b012e1d3157e80131b468abe2f3c3754259094f7
Kirill <91422323+k1dobu3@users.noreply.github.com>
2021-10-31 18:39:47 +0300

    Add files via upload

---
+++ Treygol/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Treygol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину катета прямоугольного треугольниик для его построения:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int a = 2;
+            int b = 1;
+            string xx = "";
+            string yy = " ";
+            string zz = "*";
+            string hh = "*";
+            string gg;
+            Console.WriteLine("*");
+
+            do
+            {
+                a += 1;
+                Console.WriteLine($"*{xx}*");
+                xx += yy;
+            } while (a < m);
+
+            while (b !=m)
+            {
+                zz +=hh;
+                b += 1;
+            }
+            Console.WriteLine(zz);
+        }
+    }
+}
